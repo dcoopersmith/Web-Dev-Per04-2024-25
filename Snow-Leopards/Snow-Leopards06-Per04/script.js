c1f75328282b51aef3db57f6a11738650d637fde
David Coopersmith <dcoopersmith@mvusd.net>
2024-12-05 12:13:11 -0800

    began the script.js file for the quiz

---
+++ Snow-Leopards/Snow-Leopards06-Per04/script.js
@@ -0,0 +1,16 @@
+alert("This is an alert!");
+
+
+
+
+
+
+const Questions = [
+    {
+        q: "Fill in the blank: Snow leopards live ____.",
+        a:  [{text: "in the desert.", isCorrect: false},
+             {text: "in the jungle.", isCorrect: false}
+            ]
+    }
+
+];
