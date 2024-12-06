d24beac2c03b26fac34a99540d8ef230af2c4607
David Coopersmith <dcoopersmith@mvusd.net>
2024-12-06 12:20:01 -0800

    began writing javascript for quiz

--- Snow-Leopards/Snow-Leopards06-Per04/script.js
+++ Snow-Leopards/Snow-Leopards06-Per04/script.js
@@ -1,16 +1,41 @@
-alert("This is an alert!");
-
-
-
-
-
-
+// JSON (JavaScript Object Notation)
 const Questions = [
     {
         q: "Fill in the blank: Snow leopards live ____.",
         a:  [{text: "in the desert.", isCorrect: false},
-             {text: "in the jungle.", isCorrect: false}
+             {text: "in the jungle.", isCorrect: false},
+             {text: "in the mountains.", isCorrect: true},
+             {text: "in the city.", isCorrect: false},
             ]
     }
 
 ];
+
+let currQuestion = 0;
+let score = 0;
+
+function loadQues() 
+{
+    const question = document.getElementById("ques");
+    const opt = document.getElementById("opt");
+
+    question.textContent = Questions[currQuestion].q;
+    opt.innerHTML = "";
+
+    for ( let i = 0; i < Questions[currQuestion].a.length; i++ )
+    {
+        const choicesdiv = document.createElement("div");
+        const choice = document.createElement("input");
+        const choicelabel = document.createElement("label");
+
+        choice.type = "radio";
+        choice.name = "answer";
+        choice.value = i;
+
+        choicelabel.textContent = Questions[currQuestion].a[i].text;
+        choicesdiv.appendChild(choice);
+    }
+}
+
+loadQues();
+
